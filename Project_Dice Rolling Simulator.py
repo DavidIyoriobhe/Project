ac58247f4dce2df4612cc98b946a2248491104a8
DavidIyoriobhe <42247292+DavidIyoriobhe@users.noreply.github.com>
2018-09-12 18:14:04 +0100

    Add files via upload

---
+++ Project_Dice Rolling Simulator.py
@@ -0,0 +1,10 @@
+#This is a program to construct a dice rolling simulator
+import random
+min = 1
+max = 6
+rollAgain = "yes"
+while rollAgain == "yes" or rollAgain == "y":
+    print("Rolling the dices ")
+    print ("The values are ")
+    print (random.randint(min,max))
+    rollAgain = input("Roll the dices again?")
